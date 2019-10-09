1024c30ab0f20c9e439cc5eb3561502a20276b34
Elvis Blanco <elvisblanco1993@gmail.com>
2019-10-08 22:12:58 -0400

    Auto Update works now

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require("electron-updater");
 
 let win
 
@@ -18,7 +19,32 @@ function createWindow () {
   })
   win.setMenuBarVisibility(false)
 
-  win.loadURL(`file://${__dirname}/app/app.html`)
+  win.loadURL(`file://${__dirname}/app/app.html#v${app.getVersion()}`)
+
+  // Auto updater
+  autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+  })
+  autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+  })
+  autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+  })
+  autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+  })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+  })
+  autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+  });
+  // End of Updater Code
+
 
   win.on('ready-to-show', () => {
     win.show()
